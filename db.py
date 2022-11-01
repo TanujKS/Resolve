8a9262533682534bfb37bc3c0da931442f695795
Tanuj Siripurapu <tanujsiripurapu@gmail.com>
2022-11-01 11:48:25 -0700

    Delete db.py

--- db.py
+++
@@ -1,6 +0,0 @@
-import firebase_admin
-from firebase_admin import firestore
-
-# Application Default credentials are automatically created.
-app = firebase_admin.initialize_app()
-db = firestore.client()
